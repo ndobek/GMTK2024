3c0bf43456c10fbeda463b133a310f92587a8a12
Fano <n.j.dobek@gmail.com>
2024-08-17 17:26:12 -0600

    pistons working

--- Components/piston/Piston.gd
+++ Components/piston/Piston.gd
@@ -1,21 +1,33 @@
 extends Node2D
 
 @export var spring : DampedSpringJoint2D
+var open :bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Open()
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
 
 func Open():
-	spring.rest_length = 90
-	pass
-
+	spring.rest_length = 500
+	open = true
+	print(spring.rest_length)
+	
 func Close():
 	spring.rest_length = 0
-	pass
+	open = false
+	print(spring.rest_length)
+
+
+func Toggle():
+	if open: Close()
+	else: Open()
+
+
+
+
+func ToggleEvent(viewport: Node, event: InputEvent, shape_idx: int):
+	if event.is_action_pressed("Activate"): 
+		Toggle()
+		viewport.set_input_as_handled()
